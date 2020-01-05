fc786cb1eba119b7de198de0209d27b1ea827e12
Mattias Forsman <forsman1991@gmail.com>
2020-01-05 19:13:18 +0100

    Added terms confirmation

--- app/views/frontend/register.php
+++ app/views/frontend/register.php
@@ -14,40 +14,47 @@
 			<div class="row">
 				<div class="input-field col s6">
 					<label for="first_name">Förnamn</label>
-					<input type="text" name="first_name" id="first_name" class="input" />
+					<input type="text" name="first_name" id="first_name" class="input" required />
 				</div>
 				<div class="input-field col s6">
 					<label for="last_name">Efternamn</label>
-					<input type="text" name="last_name" id="last_name" class="input" />
+					<input type="text" name="last_name" id="last_name" class="input" required />
 				</div>
 			</div>
 			<div class="row">
 				<div class="input-field col s12">
 					<label for="email">Epostadress</label>
-					<input type="email" name="email" id="email" class="input" />
+					<input type="email" name="email" id="email" class="input" required />
 				</div>
 			</div>
 			<div class="row">
 				<div class="input-field col s12">
 					<label for="phone">Telefonnummer</label>
-					<input type="tel" name="phone" id="phone" class="input" />
+					<input type="tel" name="phone" id="phone" class="input" required />
 				</div>
 			</div>
 			<div class="row">
 				<div class="input-field col s12">
 					<label for="password">Lösenord</label>
-					<input type="password" name="password" id="password" class="input" />
+					<input type="password" name="password" id="password" class="input" required />
 				</div>
 			</div>
 			<div class="row">
 				<div class="input-field col s12">
 					<label for="password_confirm">Bekräfta lösenord</label>
-					<input type="password" name="password_confirm" id="password_confirm" class="input" />
+					<input type="password" name="password_confirm" id="password_confirm" class="input" required />
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="login-submit col s12 right-align">
+				<div class="col s6">
+					<label>
+						<input type="checkbox" name="terms" value="1" required>
+						<span>Jag har läst och godkänner villkoren ovan.</span>
+					</label>
+				</div>
+
+				<div class="login-submit col s6 right-align">
 					<input type="hidden" id="recaptcha_token" name="recaptcha_token" value="">
 					<?php wp_nonce_field('bkl_register'); ?>
 					<button type="submit" name="action" class="waves-effect waves-light btn" value="register">Registrera</button>
