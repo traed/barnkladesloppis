e22d9e5b639cbdee8ee123cb3a94546360d42927
Mattias Forsman <forsman1991@gmail.com>
2020-02-20 11:23:38 +0100

    Only save admin options if you're an admin

--- app/controllers/settings-controller.php
+++ app/controllers/settings-controller.php
@@ -13,11 +13,14 @@ class Settings_Controller extends Controller {
 		if($action === 'save' && wp_verify_nonce($_POST['_wpnonce'], 'bkl_settings')) {
 			update_option('bkl_sign_up_terms', wp_kses_post($_POST['sign_up_terms']));
 			update_option('bkl_registration_terms', wp_kses_post($_POST['registration_terms']));
-			update_option('bkl_email_api_key', sanitize_text_field($_POST['email_api_key']));
-			update_option('bkl_email_api_url', sanitize_text_field($_POST['email_api_url']));
-			update_option('bkl_recaptcha_site_key', sanitize_text_field($_POST['recaptcha_site_key']));
-			update_option('bkl_recaptcha_secret', sanitize_text_field($_POST['recaptcha_secret']));
 			update_option('bkl_enable_sign_up', (int)$_POST['enable_sign_up']);
+
+			if(current_user_can('administrator')) {
+				update_option('bkl_email_api_key', sanitize_text_field($_POST['email_api_key']));
+				update_option('bkl_email_api_url', sanitize_text_field($_POST['email_api_url']));
+				update_option('bkl_recaptcha_site_key', sanitize_text_field($_POST['recaptcha_site_key']));
+				update_option('bkl_recaptcha_secret', sanitize_text_field($_POST['recaptcha_secret']));
+			}
 
 			Admin::notice('Inställningar sparade!', 'success');
 
